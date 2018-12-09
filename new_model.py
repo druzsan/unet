62083ba1d3e80842e6dcb84db48e77b33286e3d8
Alexander Druz <alexandersdruz@gmail.com>
2018-12-09 09:57:09 +0100

    Implement new U-Net architecture

---
+++ new_model.py
@@ -0,0 +1,83 @@
+from keras import layers, models
+
+
+def unet(weights=None, input_shape=(256, 256, 1), classes=1, background_as_class=False):
+    """
+    Instantiates the U-Net architecture for Keras
+
+    Note that convolutions in this model have `padding='same'` instead of `padding='valid'` in reference paper
+    in order to have the same input and output shapes
+    :param weights: optional path to the weights file to be loaded (random initialization if `None`)
+    :param input_shape: optional input shape tuple
+    :param classes: optional number of classes to predict
+    :param background_as_class: whether to create additional channel for background class
+    :return: Keras model instance
+    """
+    if background_as_class is True:
+        # Add one more class for background
+        classes += 1
+        # Classes (and background) probabilities in each pixel are conditional dependent
+        top_activation = 'softmax'
+    else:
+        # Classes (and background) probabilities in each pixel are independent
+        # Some pixel is background if all classes activations in this pixel are nearly zeros
+        top_activation = 'sigmoid'
+
+    inputs = layers.Input(input_shape)
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    conv4 = layers.Dropout(0.5)(conv4)
+    pool4 = layers.MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    conv5 = layers.Dropout(0.5)(conv5)
+
+    # up6 = layers.UpSampling2D(size=(2, 2))(conv5)
+    # up6 = layers.Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up6)
+    up6 = layers.Conv2DTranspose(512, 2, strides=(2, 2), padding='valid',
+                                 activation='relu', kernel_initializer='he_normal')(conv5)
+    merge6 = layers.concatenate([conv4, up6], axis=3)
+    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+
+    # up7 = layers.UpSampling2D(size=(2, 2))(conv6)
+    # up7 = layers.Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up7)
+    up7 = layers.Conv2DTranspose(256, 2, strides=(2, 2), padding='valid',
+                                 activation='relu', kernel_initializer='he_normal')(conv6)
+    merge7 = layers.concatenate([conv3, up7], axis=3)
+    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+    # up8 = layers.UpSampling2D(size=(2, 2))(conv7)
+    # up8 = layers.Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up8)
+    up8 = layers.Conv2DTranspose(128, 2, strides=(2, 2), padding='valid', activation='relu',
+                                 kernel_initializer='he_normal')(conv7)
+    merge8 = layers.concatenate([conv2, up8], axis=3)
+    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+    # up9 = layers.UpSampling2D(size=(2, 2))(conv8)
+    # up9 = layers.Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up9)
+    up9 = layers.Conv2DTranspose(64, 2, strides=(2, 2), padding='valid', activation='relu',
+                                 kernel_initializer='he_normal')(conv8)
+    merge9 = layers.concatenate([conv1, up9], axis=3)
+    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = layers.Conv2D(classes, 1, activation=top_activation, padding='same', kernel_initializer='he_normal')(conv9)
+
+    model = models.Model(inputs, conv9)
+
+    if weights is not None:
+        model.load_weights(weights)
+
+    return model
